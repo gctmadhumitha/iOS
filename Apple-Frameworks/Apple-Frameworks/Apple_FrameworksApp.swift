298cac4bf41039fc1fc9c44f5b5bca8b20e29812
Madhumitha Loganathan <madhu@Madhumithas-MacBook-Pro.local>
2023-09-10 21:53:45 -0700

    Swift UI basic App

---
+++ Apple-Frameworks/Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Madhumitha Loganathan on 08/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FrameworkGridView()
+        }
+    }
+}
---
+++ iQuest/iQuest/UI/Colors.swift
@@ -0,0 +1,21 @@
+//
+//  Colors.swift
+//  iQuest
+//
+//  Created by Madhumitha Loganathan on 08/09/23.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    
+}
+
+enum Colors {
+    static let accentColor = UIColor.black
+
+    static let primaryColor = UIColor.black
+}
+
